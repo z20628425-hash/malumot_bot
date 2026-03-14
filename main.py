4463ee6f1e54d694f6e920aa9a54fb4bc7637937
z20628425-hash <z20628425@gmail.com>
2026-03-14 09:22:25 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+import asyncio
+import logging
+import os
+import sys
+
+import edge_tts
+
+import asyncio
+
+from aiogram import Dispatcher,Bot,F
+from aiogram.filters import Command
+from aiogram.types import Message, BotCommand
+from dotenv import load_dotenv
+load_dotenv()
+API=os.getenv('API')
+dp=Dispatcher()
+ovozlar={
+    '👨‍✈️ Sardor 🇺🇿':'uz-UZ-SardorNeural',
+    '👩‍✈️ Sardor 🇺🇿':'uz-UZ-MadinaNeural'
+}
+async def defolut(bot :Bot):
+    command=[
+        BotCommand(command='start',description='Boslab ber'),
+        BotCommand(command='help',description='Boslab ber')
+    ]
+    await bot.set_my_commands(command)
+
+@dp.message(Command('start'))
+async def start_handler(msg:Message):
+    await msg.answer("salom hush kelibsiz")
+
+@dp.message(Command('help'))
+async def start_handler(msg:Message):
+    await msg.answer("Assalomu allekum sizga qanday yordam bera olaman")
+
+@dp.message(F.text.in_('Men'))
+async def salom_handler(msg:Message):
+    await msg.answer('Salom')
+
+@dp.message()
+async def message_handler(msg:Message):
+    T=msg.text
+    if T.lower()=='seni kim yaratgan'or T.lower()=='seni kim yasagan':
+        await msg.answer('Asadbek')
+    else:
+        await msg.answer('Salom Ali')
+
+
+async def main():
+    bot=Bot(token=API)
+    await defolut(bot)
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO,stream=sys.stdout)
+    asyncio.run(main())
